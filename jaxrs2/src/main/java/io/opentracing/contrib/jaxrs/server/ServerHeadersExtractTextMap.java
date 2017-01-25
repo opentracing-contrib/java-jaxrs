879771403047f3602a63d0abc0ae00792c14fc13
Pavol Loffay <ploffay@redhat.com>
2017-01-25 16:06:43 +0100

    Use headers iterator from  opentracing.contrib:web-servlet-filter
    jax-rs depends on javax.servlet-api, therefore it makes sense to
    reuse the code (e.g. headers flat iterator) from ot:web-servlet-filter
    instrumentation.

--- jaxrs2/src/main/java/io/opentracing/contrib/jaxrs/server/ServerHeadersExtractTextMap.java
+++ jaxrs2/src/main/java/io/opentracing/contrib/jaxrs/server/ServerHeadersExtractTextMap.java
@@ -1,13 +1,11 @@
 package io.opentracing.contrib.jaxrs.server;
 
-import java.util.AbstractMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import javax.ws.rs.core.MultivaluedMap;
 
+import io.opentracing.contrib.web.servlet.filter.HttpServletRequestExtractAdapter;
 import io.opentracing.propagation.TextMap;
 
 /**
@@ -25,47 +23,12 @@ public class ServerHeadersExtractTextMap implements TextMap {
 
     @Override
     public Iterator<Map.Entry<String, String>> iterator() {
-        return new MultivaluedMapFlatIterator<>(headers.entrySet());
+        return new HttpServletRequestExtractAdapter.MultivaluedMapFlatIterator<>(headers.entrySet());
     }
 
     @Override
     public void put(String key, String value) {
         throw new UnsupportedOperationException(
                 ServerHeadersExtractTextMap.class.getName() +" should only be used with Tracer.extract()");
-    }
-
-    public static final class MultivaluedMapFlatIterator<K, V> implements Iterator<Map.Entry<K, V>> {
-
-        private final Iterator<Map.Entry<K, List<V>>> mapIterator;
-        private Map.Entry<K, List<V>> mapEntry;
-        private Iterator<V> listIterator;
-
-        public MultivaluedMapFlatIterator(Set<Map.Entry<K, List<V>>> multiValuesEntrySet) {
-            this.mapIterator = multiValuesEntrySet.iterator();
-        }
-
-        @Override
-        public boolean hasNext() {
-            if (listIterator != null && listIterator.hasNext()) {
-                return true;
-            }
-
-            return mapIterator.hasNext();
-        }
-
-        @Override
-        public Map.Entry<K, V> next() {
-            if (mapEntry == null || (!listIterator.hasNext() && mapIterator.hasNext())) {
-                mapEntry = mapIterator.next();
-                listIterator = mapEntry.getValue().iterator();
-            }
-
-            return new AbstractMap.SimpleImmutableEntry<>(mapEntry.getKey(), listIterator.next());
-        }
-
-        @Override
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
     }
 }
