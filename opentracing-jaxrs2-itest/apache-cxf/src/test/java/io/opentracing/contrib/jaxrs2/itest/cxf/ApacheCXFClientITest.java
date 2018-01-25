bc830d4410e072f2c2ca537cfc41db99d2167281
Pavol Loffay <ploffay@redhat.com>
2018-01-25 09:33:45 +0100

    Enable CXF async client concurrent test

--- opentracing-jaxrs2-itest/apache-cxf/src/test/java/io/opentracing/contrib/jaxrs2/itest/cxf/ApacheCXFClientITest.java
+++ opentracing-jaxrs2-itest/apache-cxf/src/test/java/io/opentracing/contrib/jaxrs2/itest/cxf/ApacheCXFClientITest.java
@@ -1,9 +1,7 @@
 package io.opentracing.contrib.jaxrs2.itest.cxf;
 
 import io.opentracing.contrib.jaxrs2.itest.common.AbstractClientTest;
-import java.util.concurrent.ExecutionException;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.junit.Test;
 
 /**
  * @author Pavol Loffay
@@ -13,14 +11,5 @@ public class ApacheCXFClientITest extends AbstractClientTest {
   @Override
   protected void initServletContext(ServletContextHandler context) {
     ApacheCXFHelper.initServletContext(context);
-  }
-
-  /**
-   * Does not work because client spans are not connected to parent. Apache CXF does not allow
-   * to set custom ExecutorService
-   */
-  @Test
-  @Override
-  public void testAsyncMultipleRequests() throws ExecutionException, InterruptedException {
   }
 }
