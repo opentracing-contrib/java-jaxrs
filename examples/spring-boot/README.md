f0c7743c63f4961c2eacb1251a2e87acf4aa5195
Pavol Loffay <p.loffay@gmail.com>
2018-02-08 11:40:17 +0100

    Add filter registration to example app (#73)
    * Add filter registration to example app
    
    * Add test to example

--- examples/spring-boot/README.md
+++ examples/spring-boot/README.md
@@ -10,7 +10,7 @@ $ java -jar target/opentracing-jaxrs2-example-spring-boot-exec.jar
 
 ## Example Requests
 ```shell
-$ curl -ivX GET 'http://localhost:3000/hello'
+$ curl -ivX GET 'http://localhost:3000/hello/1'
 ```
 
 More requests can be found in [integration-tests/common/../rest/TestHandler.java](../../opentracing-jaxrs2-itest/common/src/main/java/io/opentracing/contrib/jaxrs2/itest/common/rest/TestHandler.java)
--- examples/spring-boot/src/main/java/io/opentracing/contrib/jaxrs2/example/spring/boot/Configuration.java
+++ examples/spring-boot/src/main/java/io/opentracing/contrib/jaxrs2/example/spring/boot/Configuration.java
@@ -1,6 +1,9 @@
 package io.opentracing.contrib.jaxrs2.example.spring.boot;
 
 import io.opentracing.Tracer;
+import io.opentracing.contrib.jaxrs2.server.SpanFinishingFilter;
+import javax.servlet.DispatcherType;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -12,5 +15,15 @@ public class Configuration {
     @Bean
     public Tracer tracer() {
         return new LoggingTracer();
+    }
+
+    @Bean
+    public FilterRegistrationBean spanFinishingFilter(Tracer tracer) {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new SpanFinishingFilter(tracer));
+        filterRegistrationBean.setAsyncSupported(true);
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST);
+        filterRegistrationBean.addUrlPatterns("*");
+        return filterRegistrationBean;
     }
 }
