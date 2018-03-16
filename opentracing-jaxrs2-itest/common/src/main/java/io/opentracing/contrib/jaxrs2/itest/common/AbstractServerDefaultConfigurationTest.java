0b66e4c71bc4cc9f8aeb1be8ca2af70b4e971fd2
Pavol Loffay <ploffay@redhat.com>
2018-03-16 17:59:07 +0100

    Fix default test
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- opentracing-jaxrs2-itest/common/src/main/java/io/opentracing/contrib/jaxrs2/itest/common/AbstractServerDefaultConfigurationTest.java
+++ opentracing-jaxrs2-itest/common/src/main/java/io/opentracing/contrib/jaxrs2/itest/common/AbstractServerDefaultConfigurationTest.java
@@ -50,7 +50,7 @@ public abstract class AbstractServerDefaultConfigurationTest extends AbstractJet
         assertOnErrors(mockTracer.finishedSpans());
 
         MockSpan mockSpan = mockTracer.finishedSpans().get(0);
-        Assert.assertEquals("hello/{id}", mockSpan.operationName());
+        Assert.assertEquals("/hello/{id}", mockSpan.operationName());
         Assert.assertEquals(4, mockSpan.tags().size());
         Assert.assertEquals(Tags.SPAN_KIND_SERVER, mockSpan.tags().get(Tags.SPAN_KIND.getKey()));
         Assert.assertEquals(url("/hello/1"), mockSpan.tags().get(Tags.HTTP_URL.getKey()));
