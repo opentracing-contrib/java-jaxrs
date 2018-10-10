8f2c2d812fd7dd5727875521ee082324b222dde7
Pavol Loffay <p.loffay@gmail.com>
2018-10-10 15:31:21 +0200

    Migrate auto discovery test to wildfly13 (#100)
    * Migrate auto discovery test to wildfly13
    
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>
    
    * Remove some junk
    
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>
    
    * Move version to property
    
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

--- opentracing-jaxrs2-itest/auto-discovery/src/test/java/io/opentracing/contrib/jaxrs/itest/auto/discovery/JaxrsApplication.java
+++ opentracing-jaxrs2-itest/auto-discovery/src/test/java/io/opentracing/contrib/jaxrs/itest/auto/discovery/JaxrsApplication.java
@@ -2,21 +2,20 @@ package io.opentracing.contrib.jaxrs.itest.auto.discovery;
 
 import io.opentracing.mock.MockTracer;
 import io.opentracing.util.GlobalTracer;
-import io.opentracing.util.ThreadLocalScopeManager;
-import javax.naming.NamingException;
+import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 /**
  * @author Pavol Loffay
  */
+@ApplicationScoped
 @ApplicationPath("/")
 public class JaxrsApplication extends Application {
 
-    public static final MockTracer mockTracer = new MockTracer(new ThreadLocalScopeManager(),
-            MockTracer.Propagator.TEXT_MAP);
+    private static final MockTracer mockTracer = new MockTracer();
 
-    public JaxrsApplication() throws NamingException {
+    public JaxrsApplication() {
         GlobalTracer.register(mockTracer);
     }
 }
